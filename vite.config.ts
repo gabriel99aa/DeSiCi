c6da99c5ac1a92d972c4b0e76fed61708acca4cd
Gabriel Agudelo <54853427+gabriel99aa@users.noreply.github.com>
2023-06-15 10:38:32 -0500

    deploy1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://gabriel99aa.github.io/DeSiCi',
+  base: 'https://gabriel99aa.github.io/DeSiCi/',
   build: {
     sourcemap: true,
   },
